dc904ad9c4fc4ff7453bfe0dd1316295d3f2de4f
Darren Fu <darrenfu98@gmail.com>
2018-01-27 14:54:59 -0800

    added crosshair functions

--- Assets/Scripts/Crosshair.cs
+++ Assets/Scripts/Crosshair.cs
@@ -4,11 +4,46 @@ using UnityEngine;
 
 public class Crosshair : MonoBehaviour {
 
-	float speed = 0.5f;
+	public float speed = 0.5f;
+	private float myTime = 0.0f;
+	private float nextFire = 1.0f;
+	private IEnumerator coroutine;
+	private IEnumerator start;
+
 
 	public void moveCrossHair(Vector2 input) {
 		float x = transform.position.x + input.x*speed;
 		float y = transform.position.y + input.y*speed;
 		transform.position = new Vector3(x, y, 0);
+	}
+
+	private IEnumerator Delay(float waitTime) {
+		yield return new WaitForSeconds(waitTime);
+	}
+
+	void Start() {
+		start = Delay(5.0f);
+		StartCoroutine(start);
+	}
+
+	void Update() {
+
+		coroutine = Delay(0.8f);
+		myTime = myTime + Time.deltaTime;
+		if(Input.GetButton("Space") && myTime > nextFire) {
+			
+			StartCoroutine(coroutine);
+			Fire();
+			myTime = 0.0f;
+		}
+	}
+
+	public void Fire() {
+    	RaycastHit hit;
+		if (Physics.Raycast (transform.position, Vector3.forward, out hit)) {
+			if(hit.collider.tag=="Player"){
+				print("transform");
+			}
+		}
 	}
 }
