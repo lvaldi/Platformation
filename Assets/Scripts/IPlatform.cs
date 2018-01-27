460542aabe97247b3d2703f30d450e3b2eca0945
Rhiknow <rhiknow55@gmail.com>
2018-01-27 13:58:15 -0800

    Started the Platform data structure

---
+++ Assets/Scripts/IPlatform.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public interface IPlatform
+{
+    void Init(Sprite sprite);
+
+    void Death();
+
+    
+}
---
+++ Assets/Scripts/Platform.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum PlatformType
+{
+	// TODO:
+}
+
+// 
+public abstract class Platform : MonoBehaviour, IPlatform {
+
+	[SerializeField]
+    private PlatformType _platformType;
+
+    private Sprite _sprite;
+
+    private SpriteRenderer _spriteRenderer;
+
+    public virtual void Init(Sprite sprite)
+	{
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+        _sprite = sprite;
+
+        if(_spriteRenderer == null)
+		{
+            Debug.LogError("There is no sprite renderer on the Platform GameObject.");
+        }
+		if(_sprite == null)
+		{
+            Debug.LogError("The sprite is null.");
+        }
+
+        _spriteRenderer.sprite = _sprite;
+        this.gameObject.AddComponent<PolygonCollider2D>();
+    }
+
+    public virtual void Death()
+	{
+		// might not need
+	}
+
+    
+}
