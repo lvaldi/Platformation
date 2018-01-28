338a0131e1dd621901f2c032ccfda95c5a4e8aa8
Martin <m4rtin.t@gmail.com>
2018-01-27 23:30:01 -0800

    Cross hair input management

---
+++ Assets/Scripts/CrosshairInput.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrosshairInput : MonoBehaviour {
+
+	private int playerNumber;
+	private bool isTriggerDown;
+	private Crosshair crosshair;
+	public PLAYERS currentPlayer;
+
+	// Use this for initialization
+	void Start () {
+		playerNumber = (int)currentPlayer + 1;
+		isTriggerDown = false;
+		crosshair = GetComponent<Crosshair> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		crosshairMovement ("Horizontal_R_" + playerNumber,"Vertical_R_"+playerNumber);
+		shootingTrigger("Fire_"+playerNumber);
+	}
+
+
+
+	void shootingTrigger(string action){
+		if (Input.GetAxis (action) > 0 && !isTriggerDown) {
+			//TODO - Shoot button
+			crosshair.AttemptShot();
+			
+			isTriggerDown = true;
+		}
+
+		if (Input.GetAxis (action) <= 0) {
+			isTriggerDown = false;
+		}
+
+
+	}
+
+	void crosshairMovement(string horizontal, string vertical) {
+		Vector2 directionalInput = new Vector2(Input.GetAxisRaw(horizontal), Input.GetAxisRaw(vertical));
+		crosshair.moveCrossHair (directionalInput);
+	}
+}
